2e6dd613bb5a1f1efaa638d1ac2b12080fecb198
brunnovaz1 <brunnovaz@gmail.com>
2023-05-02 00:00:35 -0300

    logo pequena e ideia para menu

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,5 +1,7 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import './header.css'
+import logo0 from '../assets/logo0.jpg'
+
 
 export default function Header(props) {
   const navigate = useNavigate()
@@ -10,17 +12,20 @@ export default function Header(props) {
   }
   return (
     <header>
+      <div>
       <h1>FACIT</h1>
       <nav>
         <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/grade">Grade Horaria</NavLink></li>
           <li><NavLink to="/notas">Notas</NavLink></li>
+          <img src={logo0} alt="logomarca" width={50}/> 
           <li><NavLink to="/financeiro">Financeiro</NavLink></li>
           <li><NavLink to={`/sobre/${props.usuarioID}`}>Sobre</NavLink></li>
           <li><button className="sair" onClick={handleClick}>Sair</button></li>
         </ul>
       </nav>
+      </div>
     </header>
   )
 }
